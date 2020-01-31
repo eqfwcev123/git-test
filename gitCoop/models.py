b54bf1d45b645fac9c24917312ea07043aa7ca6c
eqfwcev123 <dohyeonee95@hotmail.com>
2020-01-31 16:03:08 +0900

    created initial model file

--- gitCoop/models.py
+++ gitCoop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class PostModel(models.Model):
+    created_date = models.DateTimeField(auto_created=True)
+    title = models.CharField(max_length=50)
+    content = models.TextField(blank=True)
